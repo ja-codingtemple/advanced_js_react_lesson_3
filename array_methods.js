a45490a61ca8f38b0d407332252527f498c84c2a
Jeremy <jeremya@codingtemple.com>
2025-03-17 10:40:46 -0600

    Lesson examples.

---
+++ array_methods.js
@@ -0,0 +1,69 @@
+/*
+WHAT ARE ARRAY METHODS?:
+
+Array methods are functions inside of JavaScript's Array object. They exist to allow you to act upon arrays and the data inside of them.
+There are many built-in methods for arrays, however, these are a few that you should be familiar with:
+- map()
+- forEach()
+- filter()
+- find()
+- includes()
+*/
+
+// Define the array.
+const names = ["brooke", "skye", "daniel", "jay", "virginia", "james", "luna", "prancer"];
+
+/*
+METHOD: map()
+This method performs an operation on each element of the existing array and uses the result to create a new array from the modifications made to the original one.
+
+EXAMPLE:
+Use map() to create a new array with uppercase versions of each name.
+*/
+console.log("\nEXAMPLE: map()")
+const uppercaseNames = names.map(name => name.toUpperCase());
+console.log(uppercaseNames)
+
+/*
+METHOD: forEach()
+This method will execute a function once 'for each' element inside of an array.
+
+EXAMPLE:
+Use forEach() to iterate over the array & convert each name to uppercase
+*/
+console.log("\nEXAMPLE: forEach()")
+names.forEach((name, index, array) => {
+    array[index] = name.toUpperCase(); // Convert each name to uppercase and update the array
+});
+console.log(names);
+
+
+/*
+METHOD: filter()
+This method creates a new array from the old one that includes only the elements that pass the provided filter operation.
+
+EXAMPLE:
+Use filter() to create a new array that only includes names from the original array that start with "J"
+*/
+console.log("\nEXAMPLE: filter()")
+const jNames = names.filter(name => name.startsWith("J"));
+console.log(jNames)
+
+/*
+METHOD: find()
+This method attempts to find the first element in an array that matches the specified criteria. It then returns the found element.
+
+EXAMPLE:
+Use find() to find the first element whose values starts with the letter P.
+*/
+console.log("\nEXAMPLE: find()")
+const firstPName = names.find(name => name.startsWith("P"));
+console.log(firstPName)
+
+/*
+METHOD: includes()
+This method attempts to detect whether an array includes a particular value. If it does, it returns True. If not, it returns False.
+*/
+console.log("\nEXAMPLE: includes()")
+const hasLuna = names.includes("Luna");
+console.log(hasLuna);
---
+++ introduction_to_json.js
@@ -0,0 +1,56 @@
+/*
+WHAT IS JSON?
+
+JSON stands for JavaScript Object Notation. It is a string format commonly used when sending data.
+JSON is not executable code.
+JSON does not contain methods.
+JSON is structured in key-value pairs. Keys are essentially property names, values are of course values for those properties.
+
+EXAMPLE OF JSON:
+
+{
+    "name": "Prancer",
+    "breed": "Miniature Schnauzer",
+    "furColor": "Salt & Pepper",
+    "favorite_activities": {
+        "activity_one": "Barking at the TV",
+        "activity_two": "Play fighting with Dad"
+    }
+}
+
+CONVERTING OBJECTS TO JSON:
+To convert an object to JSON, you must use the JSON.stringify() method.
+
+CONVERTING JSON BACK TO OBJECTS:
+To convert JSON back into an object, you must use the JSON.parse() method.
+*/
+
+// Creating a plain JavaScript object.
+const dog = {
+    name: "Prancer",
+    breed: "Miniature Schnauzer",
+    furColor: "Salt & Pepper",
+    favorite_activities: {
+        activity_one: "Barking at the TV",
+        activity_two: "Play fighting with Dad"
+    }
+};
+
+// Print out some of the object properties.
+console.log("DISPLAYING PROPERTIES FROM A JAVASCRIPT OJBECT:")
+console.log(dog.name)
+console.log(dog.favorite_activities)
+
+// Convert the object into a JSON string with stringify()
+const json = JSON.stringify(dog);
+
+// Print the JSON string to the console.
+console.log(json);
+
+// Convert JSON back into an object.
+const dogFromJSON = JSON.parse(json);
+
+// Print out the object properties again.
+console.log("\nDISPLAYING PROPERTIES FROM A JAVASCRIPT OBJECT CREATED FROM A JSON STRING:")
+console.log(dogFromJSON.name)
+console.log(dogFromJSON.favorite_activities)
